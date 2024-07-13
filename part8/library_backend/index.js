65fabe3091d0076b491489cfc2e52b50cfd830c2
Juani-tech <jperezdichiazza@gmail.com>
2024-07-13 12:42:35 -0300

    8.4, allBooks with filter query added

--- part8/library_backend/index.js
+++ part8/library_backend/index.js
@@ -106,10 +106,18 @@ const typeDefs = `
         genres: [String!]!
     }
 
+    type Author {
+        name: String!
+        id: ID!,
+        born: Int!
+        bookCount: Int!
+    }
+
     type Query {
         bookCount: Int!
         authorCount: Int!
-        allBooks: [Book!]!
+        allBooks(author: String): [Book!]!
+        allAuthors: [Author!]!
     }
 `;
 
@@ -117,7 +125,20 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) {
+        return books;
+      }
+      return books.filter((book) => book.author === args.author);
+    },
+    allAuthors: () => {
+      return authors;
+    },
+  },
+  Author: {
+    bookCount: (root) => {
+      return books.filter((book) => book.author === root.name).length;
+    },
   },
 };
 
